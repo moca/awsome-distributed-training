36f16118818b45c01e0ffc004a9377c12dcb0947
Moca Guero <411543+moca@users.noreply.github.com>
2024-03-17 00:21:46 +0100

    add my cluster configs

---
+++ 1.architectures/5.sagemaker-hyperpod/my-cluster/create_cluster_config.sh
@@ -0,0 +1,34 @@
+source env_vars
+cat > cluster-config.json << EOL
+{
+    "ClusterName": "ml-cluster",
+    "InstanceGroups": [
+      {
+        "InstanceGroupName": "controller-machine",
+        "InstanceType": "ml.g5.2xlarge",
+        "InstanceCount": 1,
+        "LifeCycleConfig": {
+          "SourceS3Uri": "s3://${BUCKET}/LifecycleScripts/base-config/",
+          "OnCreate": "on_create.sh"
+        },
+        "ExecutionRole": "${ROLE}",
+        "ThreadsPerCore": 1
+      },
+      {
+        "InstanceGroupName": "worker-group-1",
+        "InstanceType": "ml.g5.12xlarge",
+        "InstanceCount": 2,
+        "LifeCycleConfig": {
+          "SourceS3Uri": "s3://${BUCKET}/LifecycleScripts/base-config/",
+          "OnCreate": "on_create.sh"
+        },
+        "ExecutionRole": "${ROLE}",
+        "ThreadsPerCore": 1
+      }
+    ],
+    "VpcConfig": {
+      "SecurityGroupIds": ["$SECURITY_GROUP"],
+      "Subnets":["$SUBNET_ID"]
+    }
+}
+EOL
---
+++ 1.architectures/5.sagemaker-hyperpod/my-cluster/create_fsx_provisioning_params.sh
@@ -0,0 +1,15 @@
+cat > provisioning_parameters.json << EOL
+{
+  "version": "1.0.0",
+  "workload_manager": "slurm",
+  "controller_group": "controller-machine",
+  "worker_groups": [
+    {
+      "instance_group_name": "compute-nodes",
+      "partition_name": "dev"
+    }
+  ],
+  "fsx_dns_name": "${FSX_ID}.fsx.${AWS_REGION}.amazonaws.com",
+  "fsx_mountname": "${FSX_MOUNTNAME}"
+}
+EOL
